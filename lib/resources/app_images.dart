1f89ffd230a05907c2b74a9c24c3ecce74a38030
vladrudenua <rudenua.dev@gmail.com>
2022-03-23 17:11:27 +0300

    montra 0.1 added expense income assets

---
+++ lib/resources/app_images.dart
@@ -0,0 +1,11 @@
+part of 'resources.dart';
+
+class AppImages {
+  AppImages._();
+
+  static const String incomeLogo = 'assets/images/IncomeLogo.png';
+  static const String expenseArrow = 'assets/images/expenseArrow.png';
+  static const String expenseIcon = 'assets/images/expenseIcon.png';
+  static const String incomeArrow = 'assets/images/incomeArrow.png';
+  static const String notifiaction = 'assets/images/notifiaction.png';
+}
